20539b7efde5e9974358b586d0a58578913f1dbe
Grefred <133239181+Gr1X@users.noreply.github.com>
2024-12-07 19:57:10 +0700

    push

--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -5,7 +5,7 @@
             <!-- Home -->
             <li class="text-center group relative flex-shrink-0 px-2 py-2 my-2 hover:scale-105 duration-300">
                 <span class="absolute inset-x-0 top-0 h-0.5 group-hover:bg-indigo-600 w-8 left-1/2 transform -translate-x-1/2"></span>
-                <a href="" class="inline-block flex flex-col items-center group-hover:bg-slate-400 px-6 py-0.5 rounded-xl group-hover:shadow-xl text-slate-100 active:scale-95 duration-300">
+                <a href="{{route('landing')}}" class="inline-block flex flex-col items-center group-hover:bg-slate-400 px-6 py-0.5 rounded-xl group-hover:shadow-xl text-slate-100 active:scale-95 duration-300">
                     <ion-icon name="home" class="group-hover:scale-105 group-hover:size-5 size-5 my-1 duration-300"></ion-icon>
                     <p class="font-semibold text-sm">Home</p>
                 </a>
@@ -14,7 +14,7 @@
             <!-- Donation -->
             <li class="text-center group relative flex-shrink-0 px-2 py-2 my-2 hover:scale-105 duration-300">
                 <span class="absolute inset-x-0 top-0 h-0.5 group-hover:bg-indigo-600 w-8 left-1/2 transform -translate-x-1/2"></span>
-                <a href="" class="inline-block flex flex-col items-center group-hover:bg-slate-400 px-6 py-0.5 rounded-xl group-hover:shadow-xl text-slate-100 active:scale-95 duration-300">
+                <a href="{{route('donation')}}" class="inline-block flex flex-col items-center group-hover:bg-slate-400 px-6 py-0.5 rounded-xl group-hover:shadow-xl text-slate-100 active:scale-95 duration-300">
                     <ion-icon name="heart" class="group-hover:scale-105 size-5 my-1 duration-300"></ion-icon>
                     <p class="font-semibold text-sm">Donation</p>
                 </a>
@@ -23,7 +23,7 @@
             <!-- Story -->
             <li class="text-center group relative flex-shrink-0 px-2 py-2 my-2 hover:scale-105 duration-300">
                 <span class="absolute inset-x-0 top-0 h-0.5 group-hover:bg-indigo-600 w-8 left-1/2 transform -translate-x-1/2"></span>
-                <a href="" class="inline-block flex flex-col items-center group-hover:bg-slate-400 px-6 py-0.5 rounded-xl group-hover:shadow-xl text-slate-100 active:scale-95 duration-300">
+                <a href="{{route('gallery')}}" class="inline-block flex flex-col items-center group-hover:bg-slate-400 px-6 py-0.5 rounded-xl group-hover:shadow-xl text-slate-100 active:scale-95 duration-300">
                     <ion-icon name="film" class="group-hover:scale-105 size-5 my-1 duration-300"></ion-icon>                    
                     <p class="font-semibold text-sm">Story</p>
                 </a>
@@ -32,7 +32,7 @@
             <!-- Program -->
             <li class="text-center group relative flex-shrink-0 px-2 py-2 my-2 hover:scale-105 duration-300">
                 <span class="absolute inset-x-0 top-0 h-0.5 group-hover:bg-indigo-600 w-8 left-1/2 transform -translate-x-1/2"></span>
-                <a href="" class="inline-block flex flex-col items-center group-hover:bg-slate-400 px-6 py-0.5 rounded-xl group-hover:shadow-xl text-slate-100 active:scale-95 duration-300">
+                <a href="{{route('program')}}" class="inline-block flex flex-col items-center group-hover:bg-slate-400 px-6 py-0.5 rounded-xl group-hover:shadow-xl text-slate-100 active:scale-95 duration-300">
                     <ion-icon name="flag" class="group-hover:scale-105 size-5 my-1 duration-500"></ion-icon>                        
                     <p class="font-semibold text-sm">Program</p>
                 </a>
@@ -48,25 +48,6 @@
             </li>
         </ul>
         
-
-        <!-- Get Started Button -->
-        {{-- <a class="mx-4 relative group"> 
-            <button class="relative inline-block p-px font-semibold leading-6 text-white bg-gray-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95">
-                <span class="absolute inset-0 rounded-xl bg-gradient-to-r from-slate-100 via-slate-500 to-slate-900 p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100">
-                </span>
-
-                <span class="relative z-10 block px-6 py-3 rounded-xl bg-gray-500">
-                    <div class="relative z-10 flex items-center space-x-2">
-                        <span class="transition-all duration-500 group-hover:translate-x-5 text-slate-100">Get started</span>
-                        <svg class="w-6 h-6 transition-transform duration-500 group-hover:translate-x-3 group-hover" data-slot="icon" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path clip-rule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" fill-rule="evenodd"></path>
-                        </svg>
-                    </div>
-                </span>
-            </button>
-        </a>    --}}
-
-
         <div class="mx-5">
             @if(Auth::check())
                 <img id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" 
@@ -100,7 +81,21 @@
                     </div>
                 </div>
             @else
-                <a href="{{ route('login.form') }}" class="text-white bg-sky-500 px-4 py-2 rounded-md">Login</a>
+                <a href="{{ route('login.form') }}" class="mx-4 relative group"> 
+                    <button class="relative inline-block p-px font-semibold leading-6 text-white bg-gray-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95">
+                        <span class="absolute inset-0 rounded-xl bg-gradient-to-r from-slate-100 via-slate-500 to-slate-900 p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100">
+                        </span>
+        
+                        <span class="relative z-10 block px-6 py-3 rounded-xl bg-gray-500">
+                            <div class="relative z-10 flex items-center space-x-2">
+                                <span class="transition-all duration-500 group-hover:translate-x-5 text-slate-100">Get started</span>
+                                <svg class="w-6 h-6 transition-transform duration-500 group-hover:translate-x-3 group-hover" data-slot="icon" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path clip-rule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" fill-rule="evenodd"></path>
+                                </svg>
+                            </div>
+                        </span>
+                    </button>
+                </a>   
             @endif
         </div>
     </div>
