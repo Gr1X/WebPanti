bca21e689839141fbe857b7584c7dfe2da9f3b2a
Grefred <133239181+Gr1X@users.noreply.github.com>
2025-01-03 16:08:46 +0700

    mm

--- resources/views/admin/newsAdmin/edit.blade.php
+++ resources/views/admin/newsAdmin/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="m-10">
-    <h1 class="text-4xl font-bold mb-4">Edit Program</h1>
+    <h1 class="text-5xl font-bold mb-4">Edit Berita</h1>
 
     <form action="{{ route('admin.news.update', $datas->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
